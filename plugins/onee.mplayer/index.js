64855932e16cbcfbc0bbce39d0153e93e3e593fa
chen <class4cxy@126.com>
2014-04-21 21:29:27 +0800

    update mplayer

--- plugins/onee.mplayer/index.js
+++ plugins/onee.mplayer/index.js
@@ -17,11 +17,18 @@ onee.define(function () { "use strict";
 	// base method
 	var extend  = _.extend;
 	var each = _.each;
+	var indexOf = _.indexOf;
+	var uniq = _.uniq;
 	var slice = Array.prototype.slice;
 	var getAttr = Sizzle.attr;
+	var setAttr = onee.dom.setAttr;
+	var setClass = onee.dom.setClass;
 	var onEvt = onee.dom.on;
+	var fireEvt = onee.dom.fire;
 	var appendTo = onee.dom.append;
+	var setCSS = onee.dom.css;
 	var innerHTML = onee.dom.html;
+	var innerTEXT = onee.dom.text;
 	var removeNode = onee.dom.remove;
 	var Interface = onee.interface;
 	// var 
@@ -50,6 +57,10 @@ onee.define(function () { "use strict";
 /*		setInterval(function () {
 			log(audioCtx.currentTime)
 		}, 2e3)*/
+		// 音量控件模板
+		var vVolume = '<input type="range" min="0" max="100" mplayer="volume" class="vvolume" value="{volume}" />';
+		// 播放模式控件模板
+		var vPlayModel = '<a href="#playmodel" title="{title}" class="vplaymodel {key}">{title}</a>';
 
 		function mplayer(options) {
 
@@ -71,14 +82,18 @@ onee.define(function () { "use strict";
 				currentPlay : 0,
 				// 音频仪表效果
 				meter : "default",
-				// 播放模式(单曲播放-playone/单曲循环-loopone/顺序播放-playall/列表循环-loopall/随机播放-random)
-				playModel : "playone",
+				// 播放模式(单曲循环-loopone/列表循环-loopall/随机播放-random)
+				playModel : "loopall",
 				// 音频缓冲区
-				buffer : null
+				buffer : null,
+				// 音频增益控制节点/控制音量
+				gainNode : audioCtx.createGain(),
+				// 音量
+				volume : 0.8
 			});
 
 			// 获取缓存列表数据，生成播放列表
-			mplayer.renderPlaylist(this.ui.list);
+			// mplayer.renderPlaylist(this.ui.list);
 
 			// 更新播放进度
 			// var duration;
@@ -118,13 +133,53 @@ onee.define(function () { "use strict";
 				that.remove(parseInt(getAttr(this, "rel")))
 			}, "span[mplayer=remove]")
 
-			// 监听播放模式
-			onEvt(ui.playmodel, "change", function () {
-				that.playModel = this.value;
-			})
 
-			// 监听功能按钮(播放/暂停/停止/下一首/上一首)
-			each(["play", "stop", "next", "prev"], function (fn) {
+			// 播放模式字典
+			var playModels = [
+				{key : "loopone", title : "单曲循环"},
+				{key : "loopall", title : "列表循环"},
+				{key : "random", title : "随机播放"}
+			];
+			var playModelsArr = ["loopone", "loopall", "random"];
+			var currentPlayModelInfo = playModels[indexOf(playModelsArr, that.playModel)];
+			// 监听播放模式
+			onEvt(
+				ui.playmodel = appendTo(ui.userctrl, vPlayModel.replace(rtpl, function (a, b) {
+					return currentPlayModelInfo[b];
+				})),
+				"click",
+				function () {
+					var index = indexOf(playModelsArr, that.playModel);
+					if ( --index < 0 ) index = playModels.length-1;
+					var model = playModels[index];
+					var title = model.title;
+					setClass(
+						innerTEXT(
+							setAttr(this, "title", title),
+							title
+						),
+						"vplaymodel "+(that.playModel=model.key)
+					)
+				}
+			);
+
+			// 监听音量控件
+			onEvt(
+				// 动态插入音量控制节点
+				ui.volume = appendTo(ui.userctrl, vVolume.replace("{volume}", function () {return Math.sqrt(that.volume)*100})),
+				"input",
+				function () {
+				if ( that.gainNode ) {
+					var volume = parseInt(this.value) / parseInt(this.max);
+					// Let's use an x*x curve (x-squared) since simple linear (x) does not
+					// sound as good.
+					// log(volume * volume)
+					that.volume = that.gainNode.gain.value = volume * volume;
+				}
+			});
+
+			// 监听功能按钮(播放/暂停/下一首/上一首)
+			each(["play", "next", "prev"], function (fn) {
 				onEvt(ui[fn], "click", proxy(that[fn], that));
 			});
 
@@ -133,6 +188,10 @@ onee.define(function () { "use strict";
 		extend(mplayer.prototype, {
 			add : function (input) {
 				var playlist = mplayer.playlist;
+				// 此次添加动作之前，播放列表是否为空
+				// 若为空，则添加完之后自动开始播放
+				// 若不为空，添加完之后不做任何操作
+				var isEmpty = !playlist.length;
 				// log(input)
 				each(input.files, function (file) {
 					// log(Object.prototype.toString.call(file))
@@ -141,11 +200,17 @@ onee.define(function () { "use strict";
 	            		file : file
 	            	}
 	            });
-	            mplayer.renderPlaylist(this.ui.list)
+	            // 去重
+	            mplayer.playlist = uniq(playlist, "name");
+	            // 生成列表
+	            mplayer.renderPlaylist(this.ui.list);
+	            isEmpty && this.play()
 			},
 			play : function (index) {
 
-				var file, that = this;
+				var file, that = this, playlist = mplayer.playlist;
+				// 如果播放列表为空，一律触发input[type=file]的click事件
+				if ( playlist.length === 0 ) return fireEvt(this.ui.add[0], "click");
 				// index存在，则清空当前的，直接播放mplayer.playlist[index]
 				if ( index !== undefined ) {
 					file = mplayer.playlist[that.currentPlay = index].file;
@@ -171,9 +236,7 @@ onee.define(function () { "use strict";
 						// 重新获取全局时间戳
 						// that.startTime = audioCtx.currentTime;
 						// log(that.sourceNode)
-						// 重新新建音频节点
-						that.sourceNode = null;
-						that.sourceNode = mplayer.createBufferSource(that.buffer, proxy(mplayer.onPlayEnd, that));
+						mplayer.createBufferSource.call(that);
 						// that.sourceNode.start(0);
 						that.sourceNode.start(0, that.offsetTime);
 						// goon meter drawer
@@ -191,9 +254,11 @@ onee.define(function () { "use strict";
 						// that.startTime = audioCtx.currentTime;
 						// playing
 						that.status = "playing";
+						that.buffer = buffer
 
 						// 新建音频节点
-						that.sourceNode = mplayer.createBufferSource(that.buffer = buffer, proxy(mplayer.onPlayEnd, that));
+						mplayer.createBufferSource.call(that);
+						// that.sourceNode = mplayer.createBufferSource(that.buffer = buffer, proxy(mplayer.onPlayEnd, that));
 						that.sourceNode.start(0);
 				        // sourceNode.onended = proxy(that.stop, that);
 
@@ -253,33 +318,36 @@ onee.define(function () { "use strict";
 					// call the playlist render
 					mplayer.renderPlaylist(innerHTML(this.ui.list, ""))
 				}
+				if ( playlist.length === 0 ) {
+					setCSS(this.ui.add[0], {
+						display : "block"
+					});
+					setCSS(this.ui.list, {
+						display : "none"
+					})
+				}
 			}
 		})
 
+		// 播放列表模板
 		var playListTpl = '<li><a href="#" mplayer="play" rel={index}>{num}.{name}</a><span mplayer="remove" rel={index}>X</span></li>';
 		var rtpl = /\{(.*?)\}/g;
 		var rSuffix = /\.\w+$/;
 		// 静态属性
 		extend(mplayer, {
-			// 播放列表存储，读取localStorage数据
-			playlist : JSON.parse(localStorage.getItem("mplayer-user-playlist")||"[]"),
+			// 播放列表
+			// [NOTE] 由于浏览器安全机制，无法实现播放列表真正固化
+			playlist : [],
 			// 当播放结束，用于相对应播放模式的逻辑控制
 			onPlayEnd : function () {
 				// note: when stop function or playend will disapatch it
 		        // i just need it dispatch by playend
-		        // log("doo")
-		        // log(this.offsetTime)
 	        	if (this.buffer.duration<=this.offsetTime) {
 	        		this.stop();
 	        		switch( this.playModel ) {
-	        			case "playone" :
-	        			break;
 	        			case "loopone" :
 		        			this.currentPlay--;
 		        			this.next();
-	        			break;
-	        			case "playall" :
-	        				this.currentPlay+1 < mplayer.playlist.length && this.next();
 	        			break;
 	        			case "loopall" :
 	        				this.next();
@@ -303,23 +371,40 @@ onee.define(function () { "use strict";
 	            		return item[b];
 	            	}))
 	            });
+	            if ( mplayer.playlist.length ) {
+
+	            }
 			},
 			// 新建音频节点
-			createBufferSource : function (buffer, onended) {
+			createBufferSource : function () {
 
-				var sourceNode = audioCtx.createBufferSource();
+				// 不知道新建会不会之前造成什么不良反应
+				// 手动清掉
+				if (this.sourceNode) {
+					this.sourceNode.disconnect(this.gainNode);
+					this.gainNode.disconnect(audioCtx.destination);
+					this.sourceNode = null;
+				}
+
+				this.sourceNode = audioCtx.createBufferSource();
 		        //then assign the buffer to the buffer source node
-		        sourceNode.buffer = buffer;
-		        sourceNode.connect(audioCtx.destination);
+		        this.sourceNode.buffer = this.buffer;
+		        // 重新连接增益节点
+		        this.sourceNode.connect(this.gainNode);
+		        // this.sourceNode.connect(audioCtx.destination);
+
+		        this.gainNode.connect(audioCtx.destination);
+		        this.gainNode.gain.value = this.volume
+
 		        // fix in old browsers
-		        if (!sourceNode.start) {
-		            sourceNode.start = sourceNode.noteOn;
-		            sourceNode.stop = sourceNode.noteOff;
+		        if (!this.sourceNode.start) {
+		            this.sourceNode.start = this.sourceNode.noteOn;
+		            this.sourceNode.stop = this.sourceNode.noteOff;
 		        }
 
 		        // on end
-		        sourceNode.onended = onended;
-		    	return sourceNode;
+		        this.sourceNode.onended = proxy(mplayer.onPlayEnd, this);
+		    	// return sourceNode;
 			},
 			// 读取本地文件
 			fileReader : (function () {
@@ -382,9 +467,9 @@ onee.define(function () { "use strict";
 					var 
 						analyser = audioCtx.createAnalyser(),
 						// that           = this,
-						meterNum          = 66,
 						cwidth            = ctx.canvas.width,
-						cheight           = ctx.canvas.height,
+						cheight           = ctx.canvas.height-2,
+						meterNum          = cwidth/8|0,
 						//width of the meters in the spectrum;
 						meterWidth        = 6,
 						capHeight         = 2,
@@ -395,10 +480,14 @@ onee.define(function () { "use strict";
 					//connect the source to the analyser
 			        sourceNode.connect(analyser);
 			        // set style of bar
-					var gradient = ctx.createLinearGradient(0, 0, 0, 300);
+					var gradient = ctx.createLinearGradient(0, -300, 0, 0);
 			        gradient.addColorStop(1, '#0f0');
 			        gradient.addColorStop(0.5, '#ff0');
 			        gradient.addColorStop(0, '#f00');
+
+			        // 先保存当前状态，以免下次translate递增
+			        ctx.save();
+			        ctx.translate(0, cheight)
 			        // ctx.translate(0, cheight-100);
 
 			        var _outer_ = function _inner_ () {
@@ -406,26 +495,29 @@ onee.define(function () { "use strict";
 				        autoAnimationHandle = requestAnimationFrame( _inner_ );
 
 				        var array = new Uint8Array(analyser.frequencyBinCount);
+				        // var meterNum = array.length;
 			            analyser.getByteFrequencyData(array);
 			            
 			            var step = Math.round(array.length / meterNum); //sample limited data from the total array
-			            ctx.clearRect(0, 0, cwidth, cheight);
+			            ctx.clearRect(0, 0, cwidth, -cheight);
 			            for (var i = 0; i < meterNum; i++) {
-			                var value = array[i * step];
+			                var value = array[i*step];
+			                var rwidth = i*8;
 			                if (capYPositionArray.length < meterNum) {
 			                    capYPositionArray.push(value);
 			                }
-			                ctx.fillStyle = capStyle;
-			                // console.log(value)
-			                //draw the cap, with transition effect
-			                if (value < capYPositionArray[i]) {
-			                    ctx.fillRect(i * 8, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
-			                } else {
-			                    ctx.fillRect(i * 8, cheight - value, meterWidth, capHeight);
-			                    capYPositionArray[i] = value;
-			                };
+			                // log(value)
+							ctx.fillStyle = capStyle;
+							// console.log(value)
+							//draw the cap, with transition effect
+							if (value < capYPositionArray[i]) {
+							    ctx.fillRect(rwidth, -capYPositionArray[i]--, meterWidth, capHeight);
+							} else {
+							    ctx.fillRect(rwidth, -value, meterWidth, capHeight);
+							    capYPositionArray[i] = value;
+							};
 			                ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
-			                ctx.fillRect(i * 8 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
+			                ctx.fillRect(rwidth, -value+capHeight, meterWidth, cheight); //the meter
 			            }
 			            return _inner_
 
@@ -434,10 +526,12 @@ onee.define(function () { "use strict";
 				    return {
 				    	stop : function () {
 				    		cancelAnimationFrame(autoAnimationHandle);
-				    		ctx.clearRect(0, 0, cwidth, cheight);
+				    		ctx.clearRect(0, 0, cwidth, -cheight);
 				    		autoAnimationHandle = null;
 				    		sourceNode.disconnect(analyser);
-				    		analyser.disconnect(audioCtx.destination);
+				    		// 每次暂停后就恢复translate之前
+				    		ctx.restore();
+				    		// analyser.disconnect(audioCtx.destination);
 				    		analyser = null;
 				    		gradient = null
 				    	},
@@ -458,18 +552,14 @@ onee.define(function () { "use strict";
 
 		// 监听浏览器关闭动作
 		onEvt(window, "beforeunload", function () {
-			log("beforeunload")
+			// log("beforeunload")
 			// 存储当前用户习惯
 			localStorage.setItem("mplayer-user-custom", JSON.stringify({
-				currentPlay : cfg.currentPlay,
+				// currentPlay : cfg.currentPlay,
 				playModel : cfg.playModel,
 				meter : cfg.meter,
-				volume : cfg.volume,
+				volume : cfg.volume
 			}))
-
-			// 存储当前用户播放列表
-			// log(JSON.stringify(mplayer.playlist))
-			// localStorage.setItem("mplayer-user-playlist", JSON.stringify(mplayer.playlist))
 
 		});
 
