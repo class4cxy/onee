64e1369313ce9770240d52462637cce8839a7647
chen <class4cxy@126.com>
2014-08-29 00:34:27 +0800

    add grunt

---
+++ plugins/onee.mobile.mplayer/Gruntfile.js
@@ -0,0 +1,33 @@
+﻿module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean : {
+      src : ['src/disk']
+    },
+    concat : {
+      disk : {
+        src : ['src/*.js'],
+        dest : 'src/online/seed.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> by jdo. */\n'
+      },
+      build: {
+        src: 'src/online/seed.js',
+        dest: 'src/online/seed.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat', 'uglify']);
+
+};
